b47f3b3651b3667a620f842645d8b7d9b56feef4
fineliving <liyang_qifengle@126.com>
2020-08-04 22:43:01 +0800

    文档结构调整

---
+++ docs/.vuepress/sidebars/thought.js
@@ -0,0 +1,22 @@
+// // CSS(3)的知识图谱
+// module.exports = [
+//   {
+//     title: 'HTML(5)', // 必要的
+//     path: '/basic/html/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+//     // collapsable: false, // 可选的, 默认值是 true,
+//     sidebarDepth: 1, // 可选的, 默认值是 1
+//     children: ['/basic/html/html代码规范', '/basic/html/html_语义化'],
+//   },
+//   {
+//     title: 'CSS(3)',
+//     path: '/basic/css/',
+//     sidebarDepth: 1,
+//     children: ['/basic/css/css代码规范'],
+//   },
+//   {
+//     title: 'JavaScript(ES6+)',
+//     path: '/basic/js/',
+//     sidebarDepth: 1,
+//     children: [],
+//   },
+// ]
