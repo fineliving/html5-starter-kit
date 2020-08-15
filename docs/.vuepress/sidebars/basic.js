fd439a45e30017e418188722bf7efe36bfd6d3e9
fineliving <liyang_qifengle@126.com>
2020-08-15 18:45:53 +0800

    编程基础结构整理

--- docs/.vuepress/sidebars/basic.js
+++ docs/.vuepress/sidebars/basic.js
@@ -9,22 +9,84 @@ module.exports = [
     children: [
       {
         title: '元素',
+        path: '/basic/html/元素',
         children: ['/basic/html/块元素列表', '/basic/html/行内元素列表', '/basic/html/可变元素列表', '/basic/html/空元素列表', '/basic/html/行内元素&块状元素&行内块状元素'],
       },
+      {
+        title: 'DOM',
+        path: '/basic/html/元素',
+        children: ['/basic/html/html语义化'],
+      },
+      {
+        title: '语义化',
+        path: '/basic/html/元素',
+        children: ['/basic/html/html语义化'],
+      },
       '/basic/html/html代码规范',
-      '/basic/html/html语义化',
     ],
   },
   {
     title: 'CSS(3)',
     path: '/basic/css/',
     sidebarDepth: 1,
-    children: ['/basic/css/css代码规范', '/basic/css/css样式的书写顺序及原理', '/basic/css/浏览器渲染原理及流程', '/basic/css/关键渲染路径', '/basic/css/居中', '/basic/css/垂直居中'],
+    children: [
+      {
+        title: 'CSSOM',
+        children: [],
+      },
+      {
+        title: '选择器',
+        children: [],
+      },
+      {
+        title: '布局',
+        children: [],
+      },
+      {
+        title: '交互',
+        children: [],
+      },
+      '/basic/css/css代码规范',
+      '/basic/css/css样式的书写顺序及原理',
+      '/basic/css/浏览器渲染原理及流程',
+      '/basic/css/关键渲染路径',
+      '/basic/css/居中',
+      '/basic/css/垂直居中',
+    ],
   },
   {
     title: 'JavaScript(ES6+)',
     path: '/basic/js/',
     sidebarDepth: 1,
-    children: [],
+    children: [
+      {
+        title: '类型',
+        children: [],
+      },
+      {
+        title: '原型链',
+        children: [],
+      },
+      {
+        title: '作用域',
+        children: [],
+      },
+      {
+        title: '闭包',
+        children: [],
+      },
+      {
+        title: '事件',
+        children: [],
+      },
+      {
+        title: '异步',
+        children: [],
+      },
+      {
+        title: '正则表达式',
+        children: [],
+      },
+    ],
   },
 ]
